c8403cc0911ca601127d4ad17502c8a706a6659c
delletron <32048189+delletron@users.noreply.github.com>
2017-10-18 19:21:19 -0400

    catchup

---
+++ class_work/first-javascript/script.js
@@ -0,0 +1,28 @@
+var num1 = 10*10;
+var num2 = 100/10;
+var num3 = 50-7;
+var num4 = 50*7;
+var name = 'adelle hernandez'
+
+
+
+document.write (num1);
+document.write (num2);
+document.write (num3);
+document.write (num4);
+document.write (name);
+
+
+function hello () {
+	  var greeting = 'hello!';
+
+	  greeting = 'hello, world!';
+
+	  alert(greeting);
+	}
+
+hello ();
+
+document.querySelector('#title').style.color='red';
+document.querySelector('#title').style.textDecoration='underline';
+document.querySelector('#text-chunk').style.color='blue';
