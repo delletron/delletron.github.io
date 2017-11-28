5efc37e998f9a3f5bdc6f3de40f9dfa89e9f5575
delletron <32048189+delletron@users.noreply.github.com>
2017-11-27 23:12:07 -0500

    Grow_trees_check

--- GA Final/grow_trees/js/app.js
+++ GA Final/grow_trees/js/app.js
@@ -5,8 +5,52 @@
 		//$('img').show()
 //	}
 
-$('#first-tree').click(reveal)
+
+$(function() {
+
+  $('.pop-up').hide();
+  $('.pop-up').fadeIn(1000);
+  $('.close-button').click(function (e) {
+  $('.pop-up').fadeOut(700);
+  $('#overlay').removeClass('blur-in');
+  $('#overlay').addClass('blur-out');
+  e.stopPropagation();
+  });
+});
+
+
+$('#first-add').click(reveal)
 
 function reveal () {
-	$('#first-tree').show();
+	$('#first-tree').show(slow);
+}
+
+$('#second-add').click(reveal)
+
+function reveal () {
+	$('#second-tree').show();
+}
+
+$('#third-add').click(reveal)
+
+function reveal () {
+	$('#third-tree').show();
+}
+
+$('#fourth-add').click(reveal)
+
+function reveal () {
+	$('#fourth-tree').show();
+}
+
+$('#fifth-add').click(reveal)
+
+function reveal () {
+	$('#fifth-tree').show();
+}
+
+$('#sixth-add').click(reveal)
+
+function reveal () {
+	$('#sixth-tree').show();
 }
